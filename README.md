397843844dc5669df042a60d335c4b8fa173941f
Michael Karouzos & Peter Soderberg <pair+multiviouskalogram.psoderberg@devbootcamp.com>
2014-11-21 14:49:02 -0600

    Refactored gawk controller and api/result parser into separate models

---
+++ README.md
@@ -0,0 +1,25 @@
+# Stack Overlord
+
+Stack Overlord aims to provide you with a comprehensive service that dynamically responds to all of your error message handling needs.
+
+Our company mission is to be the final word in solution-based solutions designed explicitly for creating solutions to your solution requiring application. (For profit.)
+
+Stack Overlord Manifesto:
+
+1. Include the stackoverlord gem in your pathetic, error-ridden project.
+
+2. Throw an error.
+
+3. Run the app.
+
+3. Follow the provided link.
+
+4. ... ?
+
+5. Profit!
+
+## Documentation
+
+address:mash is a hashed mac address
+
+gawk is the raised exception stored in the database (You cannot call classes Exception or Error. Those are taken.)
---
+++ app/models/stack_overflow_api_client.rb
@@ -0,0 +1,30 @@
+require 'json'
+
+class StackOverFlowApiClient
+  def initialize(query)
+    escaped_query = URI.escape(query)
+    @api_url = "https://api.stackexchange.com/2.2/search/advanced?page=1&pagesize=3&order=desc&sort=relevance&q=#{escaped_query}&tagged=ruby&site=stackoverflow"
+  end
+
+  def get_and_parse_page
+    page = open(@api_url) {|page| page.read }
+    @parsed_page = JSON.parse(get_page)["items"][0]#only looks at first result for now
+  end
+
+  def title
+    @parsed_page["title"].gsub("&quot;", "'")
+  end
+
+  def link
+    @parsed_page["link"]
+  end
+
+  def results
+    StackOverFlowResult.new(title, link)
+    # this will be required when getting more than one result from #get_and_parse_page
+    # results.map{|result| StackOverflowResult.new(result)}
+  end
+end
+
+
+
