7ec08ff4253bb6640267e69b40f724ec55375951
Michael Karouzos & Peter Soderberg <pair+multiviouskalogram.psoderberg@devbootcamp.com>
2014-11-23 14:47:40 -0600

    changed faye url to localhost

--- app/assets/javascripts/sockets.js
+++ app/assets/javascripts/sockets.js
@@ -1,6 +1,6 @@
 $(function() {
 	var mash = $('#gawkward').children().attr('id');
-  var faye = new Faye.Client('http://www.stackoverlord.com:9292/faye');
+  var faye = new Faye.Client('http://localhost:9292/faye');
   faye.subscribe('/' + mash, function(data) {
   	$('#gawkward').html(data)
   })
