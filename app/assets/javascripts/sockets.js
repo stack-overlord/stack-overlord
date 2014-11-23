79a5239a0e4a74d87c17b75592b3109bc60b5c15
Richard Baptist <rpbaptist@gmail.com>
2014-11-23 16:59:32 -0600

    solving merge conflicts

--- app/assets/javascripts/sockets.js
+++ app/assets/javascripts/sockets.js
@@ -1,11 +1,8 @@
 $(function() {
 	var mash = $('#gawkward').children().attr('id');
-<<<<<<< HEAD
-  var faye = new Faye.Client('http://localhost:9292/faye');
-=======
-  var faye = new Faye.Client('https://afternoon-reef-8266.herokuapp.com/faye');
->>>>>>> changed faye url
-  faye.subscribe('/' + mash, function(data) {
-  	$('#gawkward').html(data)
-  })
+  var gawkSubscribe = new Firebase('https://blazing-fire-7163.firebaseio.com/' + mash);
+
+  gawkSubscribe.on('child_changed', function(data) {
+  $('#gawkward').html(data.val());
+  });
 });
