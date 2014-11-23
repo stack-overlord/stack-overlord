12f3fd00c0f000962590c9f2bcfb7abc9491758c
Nick Brown & Richard Baptist <pair+nick-brown2014.rpbaptist@devbootcamp.com>
2014-11-22 18:29:20 -0600

    now supporting up to three results in message error and error type

--- app/models/gawk_results_generator.rb
+++ app/models/gawk_results_generator.rb
@@ -6,12 +6,19 @@ class GawkResultsGenerator
   def find_solutions
     error_class_results = StackOverflowApiClient.new(@gawk.error_class).results
     message_results = StackOverflowApiClient.new(@gawk.message).results
-    [error_class_results, message_results]
+    array = [error_class_results, message_results]
+    array = array.transpose
+    return array
     # only for multiple results
     # (error_class_results + message_results).uniq
   end
 
   def save_solutions
-    find_solutions.each {|solution| @gawk.results.create(solution.attributes)}
+    array = find_solutions
+    array.each {|solution| @gawk.results.create(solution[0].attributes)}
   end
+
+  # def find_all_solutions(gawk_array)
+  #   gawk_array.each{|solution| solution.save_solution }
+  # end
 end
