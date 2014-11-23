b71c522fbb44238e5ffdff9ac1dcffea8a0d89bb
Richard Baptist <rpbaptist@gmail.com>
2014-11-23 16:56:26 -0600

    solving merge issue

--- app/assets/javascripts/sockets.js
+++ app/assets/javascripts/sockets.js
@@ -1,6 +1,10 @@
 $(function() {
 	var mash = $('#gawkward').children().attr('id');
+<<<<<<< HEAD
   var faye = new Faye.Client('http://localhost:9292/faye');
+=======
+  var faye = new Faye.Client('https://afternoon-reef-8266.herokuapp.com/faye');
+>>>>>>> changed faye url
   faye.subscribe('/' + mash, function(data) {
   	$('#gawkward').html(data)
   })
