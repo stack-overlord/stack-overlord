a776db5e4de18b1a61ad8c45d4382b369c89e03c
Nick Brown & Richard Baptist <pair+nick-brown2014.rpbaptist@devbootcamp.com>
2014-11-22 20:15:54 -0600

    removed commented code

--- app/models/gawk_results_generator.rb
+++ app/models/gawk_results_generator.rb
@@ -7,10 +7,7 @@ class GawkResultsGenerator
     error_class_results = StackOverflowApiClient.new(@gawk.error_class).results
     message_results = StackOverflowApiClient.new(@gawk.message).results
     array = [error_class_results, message_results]
-    # array = array.transpose if array.length > 1
     return array
-    # only for multiple results
-    # (error_class_results + message_results).uniq
   end
 
   def save_solutions
@@ -18,8 +15,4 @@ class GawkResultsGenerator
     array.flatten!
     array.each {|solution| @gawk.results.create(solution.attributes)}
   end
-
-  # def find_all_solutions(gawk_array)
-  #   gawk_array.each{|solution| solution.save_solution }
-  # end
 end
