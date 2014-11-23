ae0142f0995d32ed1edec19386127a1a50242fb3
Richard Baptist <rpbaptist@gmail.com>
2014-11-23 12:26:31 -0600

    Updated faye to point to stackoverlord.com

--- app/helpers/gawks_helper.rb
+++ app/helpers/gawks_helper.rb
@@ -2,7 +2,7 @@ module GawksHelper
   def broadcast(mash, partial_gawk)
     channel = "/#{mash}"
     message = {:channel => channel, :data => partial_gawk[0]}
-    uri = URI.parse("http://localhost:9292/faye")
+    uri = URI.parse("http://www.stackoverlord.com:9292/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 end
